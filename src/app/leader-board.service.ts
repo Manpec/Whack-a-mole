d6d1846674f4f8e9b668ea3ceb50b815d3490c57
Mana Pecar <mana.pecar@gmail.com>
2023-01-16 20:35:47 +0100

    implemented top10 and fastestPlayer with query

--- src/app/leader-board.service.ts
+++ src/app/leader-board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection} from '@angular/fire/compat/firestore';
 import { Player } from './Player';
 import { map } from 'rxjs';
@@ -6,47 +6,41 @@ import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class LeaderBoardService implements OnInit{
+export class LeaderBoardService {
 
   playerlist!: AngularFirestoreCollection <Player>;
+  fastestPlayer!: AngularFirestoreCollection <Player>;
   players: any; //den innehålla observable 
 
-  constructor( private __afs: AngularFirestore,
-               ) { }
+  constructor( private __afs: AngularFirestore ) { }
 
-  /* ngOnInit(): void {
-    this.playerlist = this.__afs.collection('players')//link to collection in DB
-
-    this.players = this.playerlist.snapshotChanges().pipe( 
-    map(action=> { 
-      return action.map( a => {
-        const data = a.payload.doc.data() as Player; //Extract the content of the Document = object of <Player>
-        const id = a.payload.doc.id; //Extract id for this document
-        console.log(id, data);
-        
-        return { id, data };
-        
-      })
-    })
-  );
-  } */
-
-  ngOnInit(): void {
-    
-  }
 
   postPlayerScore(playerScore: Player){
     this.__afs.collection('players').add(playerScore)
   }
 
-  getPlayers(){
-    this.playerlist = this.__afs.collection('players');
+  getFastestReaction(){
+    this.fastestPlayer = this.__afs.collection('players', ref => ref.orderBy('fastestReaction', 'asc').limit(1));
+    return this.fastestPlayer.snapshotChanges().pipe(
+        map(action => {
+            return action.map(a => {
+                const data = a.payload.doc.data() as Player;
+                const id = a.payload.doc.id; 
+                console.log(id, data);
+                return { id, data };
+                
+            });
+        })
+    );
+  }
 
+  getPlayers(){
+    this.playerlist = this.__afs.collection('players', ref => ref.orderBy('score', 'desc').limit(10)); //order the collection by score field in descending order
     return this.playerlist.snapshotChanges().pipe(
         map(action => {
             return action.map(a => {
                 const data = a.payload.doc.data() as Player;
-                const id = a.payload.doc.id; //Extract id for this document
+                const id = a.payload.doc.id; 
                 console.log(id, data);
                 return { id, data };
                 
